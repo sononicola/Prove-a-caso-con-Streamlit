84f8d39e30e4ef925df3b57405da1dcf83dc1503
Nicola Meoli <46779155+sononicola@users.noreply.github.com>
2022-02-03 12:18:47 +0100

    possibile input di un  load comb

--- main.py
+++ main.py
@@ -6,11 +6,46 @@ st.header("Load Comb")
 
 nCarichi = st.number_input("n carichi", min_value=1, value=3)
 carichi = ["g1","g2","snow"]
+sfav_or_fav = ["sfav" , "fav"]
 
-c1, c2, c3 = st.columns(3)
+c1, c2, c3, c4 = st.columns([1,0.8,0.6,0.5])
 with c1:
-    a = [st.number_input(f"ciao {i+1}",min_value=1, max_value=4,key=i) for i in range(0,nCarichi)]
+    a = [
+        st.text_input(
+            f"Nome {i}",
+            value = "Load Name " + str(i),
+            key = "LoadName" + str(i)
+            ) 
+        for i in range(1,nCarichi+1)
+        ]
 with c2:
-    b = [st.selectbox(f"Carico {i+1}", options=carichi, key=i) for i in range(0,nCarichi)]
+    b = [
+        st.number_input(
+            f"Valore {i}",
+            min_value = 1.,
+            step = 1.,
+            format = "%.4f",
+            key = "LoadValue" + str(i)
+            ) 
+        for i in range(1,nCarichi+1)
+        ]
+with c3:
+    c = [
+        st.selectbox(
+            f"Tipologia {i}", 
+            options = carichi, 
+            key = "LoadType" + str(i)            
+            ) 
+        for i in range(1,nCarichi+1)
+        ]
+with c4:
+    d = [
+        st.selectbox(
+            f"Sfav or Fav {i}", 
+            options = sfav_or_fav, 
+            key = "Load_sfav_or_fav" + str(i)            
+            ) 
+        for i in range(1,nCarichi+1)
+        ]
     
-st.dataframe(pd.DataFrame(list(zip(a,b))))
+st.table(pd.DataFrame(list(zip(a,b,c,d))))
